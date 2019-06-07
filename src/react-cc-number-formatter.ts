c94b3d3f3100433339808bc7c65bcb8987ecb1e5
osaario <osaario1@gmail.com>
2019-06-07 17:14:06 +0300

    export types

--- src/react-cc-number-formatter.ts
+++ src/react-cc-number-formatter.ts
@@ -1,6 +1,4 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-import { CreditCardNumberFormatter } from './CreditCardNumberFormatter'
-
-export default CreditCardNumberFormatter
+export * from './CreditCardNumberFormatter'
